ee656013b2d9757e7770ec349e7d8213a79e52ab
Douglas da Costa <Toddynotes@gmail.com>
2018-09-18 01:03:47 -0300

    Added a Callback on the Room.databaseBuilder

--- app/src/main/java/com/toddywannalearn/roomsample/data/WordRoomDatabase.kt
+++ app/src/main/java/com/toddywannalearn/roomsample/data/WordRoomDatabase.kt
@@ -1,9 +1,11 @@
 package com.toddywannalearn.roomsample.data
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
+import android.os.AsyncTask
 
 
 @Database(entities = [Word::class],version = 1)
@@ -25,7 +27,35 @@ abstract class WordRoomDatabase : RoomDatabase() {
         }
         // Creates the database
         private fun buildDatabase(context: Context): WordRoomDatabase{
-            return Room.databaseBuilder(context.applicationContext,WordRoomDatabase::class.java, "word_database").build()
+            return Room.databaseBuilder(context.applicationContext,WordRoomDatabase::class.java, "word_database")
+                    .addCallback(object : RoomDatabase.Callback(){
+                        override fun onOpen(db: SupportSQLiteDatabase) {
+                            super.onOpen(db)
+                            PopulateDbAsync(instance).execute()
+                        }
+                    })
+                    .build()
         }
     }
+
+    class PopulateDbAsync : AsyncTask<Void, Void, Void>{
+
+        private val mDao: WordDao
+
+        constructor(db: WordRoomDatabase?){
+            mDao = db!!.wordDao()
+        }
+
+        override fun doInBackground(vararg params: Void?): Void? {
+            mDao.deleteAll()
+            var word = Word("Hello")
+            mDao.insert(word)
+            word = Word("Sabrina")
+            mDao.insert(word)
+            word = Word("Amo")
+            mDao.insert(word)
+            return null
+        }
+    }
+
 }
